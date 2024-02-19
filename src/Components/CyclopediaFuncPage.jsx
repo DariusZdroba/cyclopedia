75d45702a29b5cb610242c5019b6a2fded6b455b
Darius <dariuszdroba@gmail.com>
2024-02-19 15:37:47 +0200

    useref

--- src/Components/CyclopediaFuncPage.jsx
+++ src/Components/CyclopediaFuncPage.jsx
@@ -1,9 +1,62 @@
-import React from "react";
-import Instructor from "./Instructor";
+import React, { useEffect, useRef, useState } from "react";
+import InstructorFunc from "./InstructorFunc";
 import { getRandomUser } from "../Utility/api";
 
-class CyclopediaFuncPage extends React.Component {
-  constructor(props) {
+const CyclopediaFuncPage = () => {
+  const [state, setState] = useState(() => {
+    return {
+      instructor: undefined,
+      studentList: [],
+      studentCount: 0,
+      hideInstructor: false,
+    };
+  });
+  const [inputName, setInputName] = useState(() => "");
+  const [inputFeedback, setInputFeedback] = useState(() => "");
+  const totalRender = useRef(1);
+  useEffect(() => {
+    console.log("Render " + totalRender.current);
+    totalRender.current = totalRender.current + 1;
+  });
+  useEffect(() => {
+    console.log("This will be run on every reder");
+    const getUser = async () => {
+      const response = await getRandomUser();
+      setState((prevState) => {
+        return {
+          ...prevState,
+          instructor: {
+            name: response.data.first_name + " " + response.data.last_name,
+            email: response.data.email,
+            phone: response.data.phone_number,
+          },
+        };
+      });
+    };
+    if (!state.hideInstructor) getUser();
+  }, [state.hideInstructor]);
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await getRandomUser();
+      setState((prevState) => {
+        return {
+          ...prevState,
+          studentList: [
+            ...prevState.studentList,
+            { name: response.data.first_name + " " + response.data.last_name },
+          ],
+        };
+      });
+    };
+    if (state.studentList.length < state.studentCount) {
+      getUser();
+    } else if (state.studentList.length > state.studentCount) {
+      setState((prevState) => {
+        return { ...prevState, studentList: [] };
+      });
+    }
+  }, [state.studentCount]);
+  /* constructor(props) {
     super(props);
     this.state = JSON.parse(localStorage.getItem("cylcopediaState")) || {
       instructor: undefined,
@@ -13,9 +66,9 @@ class CyclopediaFuncPage extends React.Component {
       inputName: "",
       inputFeedback: "",
     };
-  }
+  } */
 
-  componentDidMount = async () => {
+  /*  componentDidMount = async () => {
     //console.log("Component Did Mount");
     if (JSON.parse(localStorage.getItem("cylcopediaState"))) {
       // this.setState(JSON.parse(localStorage.getItem("cylcopediaState")));
@@ -63,97 +116,95 @@ class CyclopediaFuncPage extends React.Component {
   componentWillUnmount() {
     // console.log("Component Will UnMount");
   }
-
-  handleAddStudent = () => {
-    this.setState((prevState) => {
+  
+  */
+  const handleAddStudent = () => {
+    setState((prevState) => {
       return {
+        ...prevState,
         studentCount: prevState.studentCount + 1,
       };
     });
   };
-
-  handleRemoveAllStudent = () => {
-    this.setState((prevState) => {
+  const handleRemoveAllStudent = () => {
+    setState((prevState) => {
       return {
+        ...prevState,
         studentCount: 0,
       };
     });
   };
 
-  handletoggleInstructor = () => {
-    this.setState((prevState) => {
+  const handletoggleInstructor = () => {
+    setState((prevState) => {
       return {
+        ...prevState,
         hideInstructor: !prevState.hideInstructor,
       };
     });
   };
 
-  render() {
-    //console.log("Render Component");
-    return (
-      <div>
-        <div className="p-3">
-          <span className="h4 text-success">Instructor &nbsp;</span>
-          <i
-            className={` bi ${
-              this.state.hideInstructor ? "bi-toggle-off" : "bi-toggle-on"
-            } btn btn-success btn-sm`}
-            onClick={this.handletoggleInstructor}
-          ></i>
-          {!this.state.hideInstructor && this.state.instructor ? (
-            <Instructor instructor={this.state.instructor} />
-          ) : null}
-        </div>
-
-        <div className="p-3">
-          <span className="h4 text-success">Feedback</span>
-          <br />
-          <input
-            type="text"
-            value={this.state.inputName}
-            placeholder="Name.."
-            onChange={(e) => {
-              this.setState({ inputName: e.target.value });
-            }}
-          ></input>{" "}
-          Value : {this.state.inputName}
-          <br />
-          <textarea
-            value={this.state.inputFeedback}
-            onChange={(e) => {
-              this.setState({ inputFeedback: e.target.value });
-            }}
-            placeholder="Feedback..."
-          ></textarea>
-          Value : {this.state.inputFeedback}
-        </div>
-        <div className="p-3">
-          <span className="h4 text-success">Students</span> <br />
-          <div>Student Count : {this.state.studentCount}</div>
-          <button
-            className="btn btn-success btn-sm"
-            onClick={this.handleAddStudent}
-          >
-            Add Student
-          </button>
-          &nbsp;
-          <button
-            className="btn btn-danger btn-sm"
-            onClick={this.handleRemoveAllStudent}
-          >
-            Remove All Students
-          </button>
-          {this.state.studentList.map((student, index) => {
-            return (
-              <div className="text-white" key={index}>
-                - {student.name}
-              </div>
-            );
-          })}
-        </div>
+  //console.log("Render Component");
+  return (
+    <div>
+      <div className="p-3">
+        <span className="h4 text-success">Instructor &nbsp;</span>
+        <i
+          className={` bi ${
+            state.hideInstructor ? "bi-toggle-off" : "bi-toggle-on"
+          } btn btn-success btn-sm`}
+          onClick={handletoggleInstructor}
+        ></i>
+        {!state.hideInstructor && state.instructor ? (
+          <InstructorFunc instructor={state.instructor} />
+        ) : null}
       </div>
-    );
-  }
-}
+
+      <div className="p-3">
+        <span className="h4 text-success">Feedback</span>
+        <br />
+        <input
+          type="text"
+          value={state.inputName}
+          placeholder="Name.."
+          onChange={(e) => {
+            setInputName(e.target.value);
+          }}
+        ></input>{" "}
+        Value : {inputName}
+        <br />
+        <textarea
+          value={inputFeedback}
+          onChange={(e) => {
+            setInputFeedback(e.target.value);
+          }}
+          placeholder="Feedback..."
+        ></textarea>
+        Value : {inputFeedback}
+      </div>
+      <div className="p-3">
+        <span className="h4 text-success">Students</span> <br />
+        <div>Student Count : {state.studentCount}</div>
+        <button className="btn btn-success btn-sm" onClick={handleAddStudent}>
+          Add Student
+        </button>
+        &nbsp;
+        <button
+          className="btn btn-danger btn-sm"
+          onClick={handleRemoveAllStudent}
+        >
+          Remove All Students
+        </button>
+        {state.studentList.map((student, index) => {
+          return (
+            <div className="text-white" key={index}>
+              - {student.name}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default CyclopediaFuncPage;
